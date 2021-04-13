a719138f1a3cee144bb46b320ff0d6f16425bde7
Leonardo Akira <leonardoakira08@gmail.com>
2021-04-13 18:30:53 -0300

    create: SocketSetup service

--- Server/Main.java
+++ Server/Main.java
@@ -1,9 +1,25 @@
 package Server;
 
+import java.net.Socket;
+
+import Server.Socket.SocketSetup;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		new Thread() {
+			public void run() {
+				try {
+					SocketSetup socketSetup = SocketSetup.getInstance(8081);
+					Socket socket = socketSetup.setUpConnection();
+					System.out.println("Connection with client made");
+					socketSetup.closeConnection();
+					System.out.println("success");
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+			}
+		}.start();
 	}
 
 }
---
+++ Server/Socket/SocketSetup.java
@@ -0,0 +1,45 @@
+package Server.Socket;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketSetup {
+	private static SocketSetup instance;
+	private ServerSocket serverSocket;
+	private int port;
+	
+	public SocketSetup(int portOptional) {
+		this.port = portOptional;
+	}
+	
+	public SocketSetup() {
+		this.port = 4000;
+	}
+	
+	public static SocketSetup getInstance(int socketPort) {
+		if(instance == null)
+			instance = new SocketSetup(socketPort);
+		
+		return instance;
+	}
+	
+	public static SocketSetup getInstance() {
+		if(instance == null)
+			instance = new SocketSetup();
+		
+		return instance;
+	}
+	
+	public Socket setUpConnection() throws Exception {
+		this.serverSocket = new ServerSocket(this.port);
+		System.out.println("Server created");
+		return serverSocket.accept();	
+	}
+	
+	public void closeConnection() throws Exception{
+		if(serverSocket == null)
+			throw new Exception("setUpConnection() must be executed beforehand");
+		
+		serverSocket.close();
+	}
+}
